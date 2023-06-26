d5d8027d07aa9a5a3514de1d312feb6f378434d6
SiddharthVarde22 <21stargames@gmail.com>
2023-06-26 23:42:17 +0530

    Added Player movements, and basic Background

---
+++ Assets/VardeSiddharth_Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    [SerializeField]
+    int maxGridXSize = 7, maxGridYSize = 7;
+
+    float horizontalInput, verticalInput;
+    Vector3 nextPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        MovePlayer();
+
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            nextPosition =  transform.position + (transform.right * horizontalInput);
+            nextPosition.x = Mathf.Clamp(nextPosition.x, 0, maxGridXSize);
+            transform.position = nextPosition;
+        }
+        else if (Input.GetButtonDown("Vertical"))
+        {
+            nextPosition = transform.position + (transform.up * verticalInput);
+            nextPosition.y = Mathf.Clamp(nextPosition.y, 0, maxGridYSize);
+            transform.position = nextPosition;
+        }
+    }
+}
